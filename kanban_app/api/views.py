3dc6b9778f0f8351a83183b38daf54751c373b3e
gloedige <gerd.loedige@posteo.de>
2026-09-11 11:33:36 +0200

    bufix get_queryset, when user is no member in view.py in kanban_app

--- kanban_app/api/views.py
+++ kanban_app/api/views.py
@@ -1,6 +1,7 @@
 
-
+from django.db.models import Q
 from rest_framework import generics, serializers, viewsets
+from .permissions import IsOwnerOrMember
 from rest_framework.permissions import IsAuthenticated
 from ..models import Board
 from .serializers import BoardSerializer, MemberSerializer, TaskSerializer
@@ -8,7 +9,12 @@ from .serializers import BoardSerializer, MemberSerializer, TaskSerializer
 class BoardsListViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
-    permission_classes = [IsAuthenticated]
-
+    permission_classes = [IsAuthenticated, IsOwnerOrMember]
+    
     def perform_create(self, serializer):
         serializer.save(owner_id=self.request.user.id)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Board.objects.filter(Q(owner=user) | Q(members__user=user)).distinct()
+            
