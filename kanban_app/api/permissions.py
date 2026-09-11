3d2356f3c68cbc27bf963b2c7556f4dfc7b4ecb1
gloedige <gerd.loedige@posteo.de>
2026-09-11 11:33:01 +0200

    order permissions.py in kanban_app

--- kanban_app/api/permissions.py
+++ kanban_app/api/permissions.py
@@ -1,17 +1,12 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.permissions import SAFE_METHODS
 
-class IsOwnerOrMemer(BasePermission):
-    def has_object_permission(self, request, view, board):
-
-        if request.method in SAFE_METHODS:
-            return True
-        if board.owner == request.user:
-            return True
-        if request.user in board.members.all():
-            return True
-        #TODO hier dürfen nur Mitglieder Änderungen vornehmen, nicht alle member
-        return board.members.filter(id=request.user.id).exists()
-
+class IsOwnerOrMember(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, board):
+        is_owner = board.owner.id == request.user.id
+        is_member = board.members.filter(id=request.user.id).exists()
+
+        return is_owner or is_member
