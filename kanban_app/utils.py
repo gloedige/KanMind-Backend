659442c3f769809811f9cad536d56904360e7ae3
gloedige <gerd.loedige@posteo.de>
2026-09-25 15:01:16 +0200

    create methode checkCommitIdIsValid and checkCommentIdExists

--- kanban_app/utils.py
+++ kanban_app/utils.py
@@ -1,4 +1,4 @@
-from kanban_app.models import Task
+from kanban_app.models import Task, Comment
 """
 Utility functions for task validation.
 Functions:
@@ -12,4 +12,14 @@ def checkTaskIdIsValid(self, task_id):
         return task_id_not_none and task_id_is_digit
 
 def checkTaskIdExists(self, task_id):
-    return Task.objects.filter(pk=task_id).exists()
+    return Task.objects.filter(id=task_id).exists()
+
+def checkCommentIdIsValid(self, comment_id):
+        print(f"Checking validity of comment ID: {comment_id}")
+        comment_id_not_none = comment_id is not None
+        comment_id_is_digit = str(comment_id).isdigit()
+        print(f"Comment ID not None: {comment_id_not_none}, Comment ID is digit: {comment_id_is_digit}")
+        return comment_id_not_none and comment_id_is_digit
+
+def checkCommentIdExists(self, comment_id):
+    return Comment.objects.filter(id=comment_id).exists()
