772f845ad496ac4ee3d3c3c767940c056fba6659
gloedige <gerd.loedige@posteo.de>
2026-09-25 10:07:46 +0200

    update urls with nested router for comments of a single task

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path, include
-from .views import BoardViewSet, TaskViewSet, EmailViewSet
+from .views import BoardViewSet, TaskViewSet, EmailViewSet, CommentViewSet
 from rest_framework import routers
+from rest_framework_nested import routers
 
 router = routers.SimpleRouter()
 router.register(r'boards', BoardViewSet, basename='boards')
 router.register(r'tasks', TaskViewSet, basename='tasks')
 router.register(r'email-check', EmailViewSet, basename='email-check')
 
+tasks_router = routers.NestedSimpleRouter(router, r'tasks', lookup='task')
+tasks_router.register(r'comments', CommentViewSet, basename='task-comments')
+
 urlpatterns = [
     path('', include(router.urls), name='boards_list'),
     path('', include(router.urls), name='tasks_list'),
     path('', include(router.urls), name='email-check'),
+    path('', include(tasks_router.urls), name='task-comments'),
 ]
